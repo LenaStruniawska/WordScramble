a02a34a21d1eba907c89504801ba4d71222ed4df
Magdalena Struniawska <mag.sok@onet.pl>
2022-03-09 08:50:01 +0100

    Initial Commit

---
+++ WordScrable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScrable
+//
+//  Created by Slawek on 09/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScrable/WordScrableApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrableApp.swift
+//  WordScrable
+//
+//  Created by Slawek on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
